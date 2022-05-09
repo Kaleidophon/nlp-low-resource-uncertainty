0c9a7c9f44013e1e3bacd0cd8376899c4118fccb
Kaleidophon <dennis.ulmer@gmx.de>
2022-05-09 15:57:35 +0200

    :art: Improve ID / OOD plots

--- scripts/check_subsampling_and_ood.py
+++ scripts/check_subsampling_and_ood.py
@@ -4,6 +4,7 @@ Create sub-sampled version of training sets and check corpus statistics to ensur
 
 # STD
 from collections import Counter
+import os
 from typing import Tuple, List, Optional
 
 # EXT
@@ -16,6 +17,7 @@ from nlp_uncertainty_zoo.utils.samplers import (
     SequenceClassificationSampler,
 )
 from torch.utils.data import DataLoader
+import pickle
 
 # PROJECT
 from src.data import DanPlusBuilder, FinnishUDBuilder, ClincPlusBuilder
@@ -47,7 +49,16 @@ PLOT_STYLE = {
     "text.usetex": True,
 }
 plt.style.use("science")
-COLORS = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+# COLORS = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+PICKLE_PATH = "scripts/subsampling_data.pkl"
+COLORS = ["orangered", "darkmagenta", "plum"]
+EDGECOLORS = ["lightsalmon", "plum", "darkmagenta"]
+HATCHES = ["..", "//", "\\\\"]
+DISPLAY_NAMES = {
+    "danplus": "Dan+",
+    "finnish_ud": "Finnish UD",
+    "clinc_plus": "Clinc Plus",
+}
 
 
 def collect_sentence_length_and_class_dict(
@@ -289,146 +300,190 @@ def plot_length_dists(
         plt.close()
 
 
+def create_id_ood_plot(dataset_names, data, x_label, target, top_n=None, sort=False):
+
+    fig, ax = plt.subplots(figsize=(10, 5), ncols=3, sharey="row")
+    width = 0.4
+    data_labels = ["ID", "OOD"]
+    x = np.arange(top_n)
+
+    for a in ax:
+        a.grid(visible=True, axis="both", which="major", linestyle=":", color="grey")
+
+    for ax_num, dataset_name in enumerate(dataset_names):
+        data_id = data[dataset_name][f"orig_{target}"]
+        data_ood = data[dataset_name][f"ood_{target}"]
+
+        if top_n is not None:
+            data_id = dict(data_id.most_common(top_n))
+            data_ood = {type_: data_ood[type_] for type_ in data_id}
+
+        # Sort keys by frequency, decendingly
+        if sort:
+            sorted_keys = list(zip(*sorted(data_id.items(), key=lambda t: t[1])))[0]
+
+        else:
+            sorted_keys = x
+
+        total_orig = sum(data_id.values())
+        total_ood = sum(data_ood.values())
+        freqs_id = np.zeros(top_n)
+        freqs_ood = np.zeros(top_n)
+
+        for i, key in enumerate(sorted_keys):
+            freqs_id[i] = data_id.get(key, 0) / total_orig
+            freqs_ood[i] = data_ood.get(key, 0) / total_ood
+
+        ax[ax_num].bar(
+            x - width / 2,
+            freqs_id,
+            label="ID",
+            width=0.3,
+            color=COLORS[0],
+            hatch=HATCHES[0],
+            edgecolor=EDGECOLORS[0],
+            linewidth=0.5,
+            alpha=0.7,
+        )
+        ax[ax_num].bar(
+            x + width / 2,
+            freqs_ood,
+            label="OOD",
+            width=0.3,
+            color=COLORS[1],
+            hatch=HATCHES[1],
+            edgecolor=EDGECOLORS[1],
+            linewidth=0.5,
+            alpha=0.7,
+        )
+        ax[ax_num].title.set_text(DISPLAY_NAMES[dataset_name])
+
+        if sort:
+            ax[ax_num].set_xticklabels(range(top_n + 5, 0, -5))
+
+    ax[1].set_xlabel(x_label, alpha=0.6)
+    ax = ax[0]
+    handles, labels = ax.get_legend_handles_labels()
+    handles = [h[0] for h in handles]
+
+    # prepare y-axis
+    ax.set_ylabel("Relative frequency", alpha=0.6)
+    ax.legend(labels=data_labels, handles=handles, loc="upper left")
+    # ax.axvline(x=2.5, c="black")
+    fig.tight_layout()
+
+    plt.savefig(
+        f"{IMG_PATH}/{target}_id_ood.pdf", format="pdf", dpi=300, bbox_inches="tight"
+    )
+
+
 if __name__ == "__main__":
+    datasets = ["danplus", "finnish_ud", "clinc_plus"]
+    data = {dataset: {"subsampled": {}} for dataset in datasets}
 
-    for dataset_name, builder, sampler_class, sampler_kwargs in zip(
-        ["danplus", "finnish_ud", "clinc_plus"],
-        [DanPlusBuilder, FinnishUDBuilder, ClincPlusBuilder],
-        [
-            TokenClassificationSampler,
-            TokenClassificationSampler,
-            SequenceClassificationSampler,
-        ],
-        [
-            SAMPLING_PARAMS_TOKEN_PRED,
-            SAMPLING_PARAMS_TOKEN_PRED,
-            SAMPLING_PARAMS_TOKEN_PRED,
-        ],
-    ):
+    if os.path.exists(PICKLE_PATH):
+        with open(PICKLE_PATH, "rb") as pkl_file:
+            data = pickle.load(pkl_file)
 
-        orig_data = builder(data_dir=DATA_DIR, num_jobs=2, max_length=MAX_LENGTH).build(
-            batch_size=BATCH_SIZE
-        )
-        (
-            orig_seq_freqs,
-            orig_token_freqs,
-            orig_label_freqs,
-        ) = collect_sentence_length_and_class_dict(
-            orig_data["train"], ignore_tokens=IGNORE_TOKENS
-        )
+    else:
+        for dataset_name, builder, sampler_class, sampler_kwargs in zip(
+            datasets,
+            [DanPlusBuilder, FinnishUDBuilder, ClincPlusBuilder],
+            [
+                TokenClassificationSampler,
+                TokenClassificationSampler,
+                SequenceClassificationSampler,
+            ],
+            [
+                SAMPLING_PARAMS_TOKEN_PRED,
+                SAMPLING_PARAMS_TOKEN_PRED,
+                SAMPLING_PARAMS_TOKEN_PRED,
+            ],
+        ):
 
-        # Perform analyses on OOD data
-        (
-            ood_seq_freqs,
-            ood_token_freqs,
-            ood_label_freqs,
-        ) = collect_sentence_length_and_class_dict(
-            orig_data["ood_test"], ignore_tokens=IGNORE_TOKENS
-        )
-        del orig_data
-
-        # Plot those results
-        # Plot sentence length distributions
-        plot_length_dists(
-            orig_seq_freqs,
-            ood_seq_freqs,
-            colors=COLORS[1::2],
-            # title="Relative sentence length frequencies",
-            save_path=f"{IMG_PATH}/{dataset_name}_seq_lengths_ood.png",
-            compare_label="ood",
-        )
-
-        # Plot token distributions
-        plot_dists(
-            orig_token_freqs,
-            ood_token_freqs,
-            colors=COLORS[1::2],
-            top_n=TOP_N,
-            # title="Relative type frequencies",
-            save_path=f"{IMG_PATH}/{dataset_name}_type_freqs_ood.png",
-            compare_label="ood",
-        )
-
-        # Plot task distributions
-        if dataset_name != "clinc_plus":
-            plot_dists(
-                orig_label_freqs,
-                ood_label_freqs,
-                x_axis="label",
-                colors=COLORS[1::2],
-                top_n=TOP_N,
-                # title="Relative type frequencies",
-                save_path=f"{IMG_PATH}/{dataset_name}_class_freqs_ood.png",
-                compare_label="ood",
-            )
-
-        # Perform the same tests for sub-samples of increasing size
-        type_coverages, token_coverages = [], []
-
-        for target_size in TARGET_SIZES:
-            subsampled_data = builder(
-                data_dir=DATA_DIR,
-                num_jobs=2,
-                max_length=MAX_LENGTH,
-                sampler_class=sampler_class,
-                sampler_kwargs={
-                    "train": {"target_size": target_size, **sampler_kwargs}
-                },
+            orig_data = builder(
+                data_dir=DATA_DIR, num_jobs=2, max_length=MAX_LENGTH
             ).build(batch_size=BATCH_SIZE)
-
             (
-                sampled_seq_freqs,
-                sampled_token_freqs,
-                sampled_label_freqs,
-            ) = collect_sentence_length_and_class_dict(
-                subsampled_data["train"], ignore_tokens=IGNORE_TOKENS
-            )
-            del subsampled_data
-
-            # Plot results
-            type_cover, token_cover = compute_coverage(
-                orig_token_freqs, sampled_token_freqs
-            )
-            type_coverages.append(type_cover)
-            token_coverages.append(token_cover)
-
-            # Plot sentence length distributions
-            plot_length_dists(
                 orig_seq_freqs,
-                sampled_seq_freqs,
-                colors=COLORS[::2],
-                top_n=TOP_N,
-                # title=f"Relative sentence length frequencies (n={target_size})",
-                save_path=f"{IMG_PATH}/{dataset_name}_seq_lengths_{target_size}.png",
-            )
-
-            # Plot token distributions
-            plot_dists(
                 orig_token_freqs,
-                sampled_token_freqs,
-                colors=COLORS[::2],
-                top_n=TOP_N,
-                # title=f"Relative type frequencies (n={target_size})",
-                save_path=f"{IMG_PATH}/{dataset_name}_type_freqs_{target_size}.png",
-            )
-
-            # Plot task distributions
-            plot_dists(
                 orig_label_freqs,
-                sampled_label_freqs,
-                x_axis="label",
-                colors=COLORS[::2],
-                top_n=TOP_N,
-                # title=f"Relative type frequencies (n={target_size})",
-                save_path=f"{IMG_PATH}/{dataset_name}_class_freqs_{target_size}.png",
+            ) = collect_sentence_length_and_class_dict(
+                orig_data["train"], ignore_tokens=IGNORE_TOKENS
             )
 
-        # Plot coverages a function of sample size
-        plot_coverage(
-            TARGET_SIZES,
-            type_coverages,
-            token_coverages,
-            colors=COLORS[::2],
-            # title="Type and token coverage of the sub-sampled vocabulary",
-            save_path=f"{IMG_PATH}/{dataset_name}_coverage.png",
-        )
+            data[dataset_name]["orig_seq_freqs"] = orig_seq_freqs
+            data[dataset_name]["orig_token_freqs"] = orig_token_freqs
+            data[dataset_name]["orig_label_freqs"] = orig_label_freqs
+
+            # Perform analyses on OOD data
+            (
+                ood_seq_freqs,
+                ood_token_freqs,
+                ood_label_freqs,
+            ) = collect_sentence_length_and_class_dict(
+                orig_data["ood_test"], ignore_tokens=IGNORE_TOKENS
+            )
+
+            data[dataset_name]["ood_seq_freqs"] = ood_seq_freqs
+            data[dataset_name]["ood_token_freqs"] = ood_token_freqs
+            data[dataset_name]["ood_label_freqs"] = ood_label_freqs
+
+            del orig_data
+
+            for target_size in TARGET_SIZES:
+                subsampled_data = builder(
+                    data_dir=DATA_DIR,
+                    num_jobs=2,
+                    max_length=MAX_LENGTH,
+                    sampler_class=sampler_class,
+                    sampler_kwargs={
+                        "train": {"target_size": target_size, **sampler_kwargs}
+                    },
+                ).build(batch_size=BATCH_SIZE)
+
+                (
+                    sampled_seq_freqs,
+                    sampled_token_freqs,
+                    sampled_label_freqs,
+                ) = collect_sentence_length_and_class_dict(
+                    subsampled_data["train"], ignore_tokens=IGNORE_TOKENS
+                )
+
+                data[dataset_name]["subsampled"][
+                    "sampled_seq_freqs"
+                ] = sampled_seq_freqs
+                data[dataset_name]["subsampled"][
+                    "sampled_token_freqs"
+                ] = sampled_token_freqs
+                data[dataset_name]["subsampled"][
+                    "sampled_label_freqs"
+                ] = sampled_label_freqs
+
+                del subsampled_data
+
+        with open(PICKLE_PATH, "wb") as pkl_file:
+            pickle.dump(data, pkl_file)
+
+    # Create the following plots
+
+    # Sequence length distributions ID / OOD for all languages side by side
+    create_id_ood_plot(
+        datasets,
+        data,
+        x_label="Sequence length",
+        top_n=40,
+        target="seq_freqs",
+        sort=False,
+    )
+
+    # Top 25 type distributions ID / OOD for all languages side by side
+    create_id_ood_plot(
+        datasets, data, x_label="Type Rank", target="token_freqs", top_n=25, sort=True
+    )
+
+    # Relative frequency of labels for finnish / danish for original and subsampled
+
+    # Relative frequency of sentence length for all languages for original and subsampled
+
+    # Relative frequency of top 25 types for all languages for original and subsampled
