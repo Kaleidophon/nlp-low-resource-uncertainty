6570f2edf1e3adb5dd54f08ddc90b65991d7ba60
Kaleidophon <dennis.ulmer@gmx.de>
2022-05-25 15:45:48 +0100

    :art: First version of scatter plot

--- visualize_results.py
+++ visualize_results.py
@@ -6,6 +6,7 @@ Produce scatter plots based on experimental results.
 import argparse
 from collections import defaultdict
 import os
+import re
 from typing import List, Dict, Optional, Tuple
 import pickle
 
@@ -20,11 +21,11 @@ from src.config import AVAILABLE_DATASETS
 # CONST
 RESULT_DIR = "./results"
 MODEL_DIR = "./models"
-IMG_DIR = "./img"
+IMG_DIR = "./img/scatter_plots"
 
 
 # Plotting defaults
-ALPHA = 0.6
+ALPHA = 0.4
 MODEL_COLORS = {
     "lstm": ("firebrick", "lightcoral"),
     "lstm_ensemble": ("forestgreen", "yellowgreen"),
@@ -44,27 +45,61 @@ METRIC_MARKERS = {
     "mutual_information": "X",
     "log_prob": "D",
 }
-TRAINING_SIZE_SCALES = {"dan+": {1000: 1, 2000: 1.2, 4000: 1.4}}
-PLOT_STYLE = {
-    "axes.facecolor": "white",
-    "figure.facecolor": "white",
-    "axes.spines.left": True,
-    "axes.spines.bottom": True,
-    "axes.spines.right": True,
-    "axes.spines.top": True,
-    "axes.edgecolor": ".15",
-    "grid.color": ".8",
-    "ytick.left": True,
-    "text.usetex": True,
-}
+TRAINING_SIZE_SCALES = {"dan+": {1000: 8, 2000: 40, 4000: 80}}
 plt.style.use("science")
 
 
 def plot_results(
     x_axis: str,
-    y_axis: str,
+    x_label: str,
+    y_label: str,
+    metric_prefix: str,
+    metrics: List[str],
     data,
-    ood_data: Optional[Dict] = None,
+    save_path: Optional[str] = None,
+    model_colors: Dict[str, Tuple[str, str]] = MODEL_COLORS,
+    metric_markers: Dict[str, str] = METRIC_MARKERS,
+    size_scales: Optional[Dict[int, float]] = None,
+):
+    fig = plt.figure(figsize=(5, 5))
+    ax = plt.gca()
+    ax.grid(visible=True, axis="both", which="major", linestyle=":", color="grey")
+
+    for model in data.keys():
+        for training_size in data[model]:
+            for metric in metrics:
+                edge_color, color = model_colors[model]
+                target_x = data[model][training_size][x_axis]
+                target_y = data[model][training_size][metric_prefix + metric]
+
+                if len(target_x) != len(target_y):
+                    continue
+
+                plt.scatter(
+                    target_x,
+                    target_y,
+                    marker=metric_markers[metric],
+                    color=color,
+                    edgecolor=edge_color,
+                    alpha=ALPHA,
+                    s=size_scales[training_size],
+                )
+
+    # ax.set_xlim([0, 1])
+    # ax.set_ylim([0, 1])
+    ax.set_xlabel(x_label, alpha=0.6)
+    ax.set_ylabel(y_label, alpha=0.6)
+    # ax.legend(labels=data_labels, handles=handles, loc="upper left")
+    # ax.axvline(x=2.5, c="black")
+    fig.tight_layout()
+
+    plt.savefig(save_path, format="pdf", dpi=300, bbox_inches="tight")
+
+
+def plot_results_id_ood(
+    x_axis: str,
+    metrics: List[str],
+    data,
     save_path: Optional[str] = None,
     model_colors: Dict[str, Tuple[str, str]] = MODEL_COLORS,
     metric_markers: Dict[str, str] = METRIC_MARKERS,
@@ -112,16 +147,69 @@ if __name__ == "__main__":
 
     # Read in data
     # Map from model to training size to ID / OOD to metric
-    data = defaultdict(
-        lambda: defaultdict(lambda: defaultdict(lambda: defaultdict[list]))
-    )
+    data = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    found_metrics = set()
 
     for result_path in result_paths:
+        _, training_size, model_name = (
+            re.compile(r"(.+?)_(\d+)_(.+)_\d{2}-\d{2}-\d{4}")
+            .match(result_path)
+            .groups()
+        )
+        training_size = int(training_size)
+
         with open(f"{args.result_dir}/{result_path}", "rb") as result_file:
             scores = pickle.load(result_file)
-            ...
 
-    # Create scatter plot: Kendall's tau as
+            for name, score in scores.items():
 
-    # TODO: Read in relevant data per dataset / model
-    # TODO: Create beautiful plots
+                if name.startswith("auroc"):
+                    found_metrics.add(name.replace("auroc_", ""))
+
+                data[model_name][training_size][name] += score
+
+    # Create plots
+    plot_results(
+        x_axis="macro_f1_scores",
+        metrics=found_metrics,
+        metric_prefix="auroc_",
+        x_label="Macro F1 score",
+        y_label="ID / OOD AUROC",
+        data=data,
+        save_path=f"{IMG_DIR}/scatter_auroc.pdf",
+        size_scales=TRAINING_SIZE_SCALES[args.dataset],
+    )
+
+    import sys
+
+    sys.exit(0)
+
+    plot_results(
+        x_axis="macro_f1",
+        metrics=[f"aupr_{metric_name}" for metric_name in found_metrics],
+        data=data,
+        save_path=f"{IMG_DIR}/scatter_aupr.pdf",
+    )
+
+    # Create ID / OOD plots
+    # TODO: Check for the kind of dataset
+    plot_results(
+        x_axis="macro_f1_scores",
+        metrics=[f"{metric_name}_kendalls_tau_token" for metric_name in found_metrics],
+        data=data,
+        save_path=f"{IMG_DIR}/scatter_kendalls_tau_token.pdf",
+    )
+    plot_results(
+        x_axis="macro_f1",
+        metrics=[f"{metric_name}_kendalls_tau_seq" for metric_name in found_metrics],
+        data=data,
+        save_path=f"{IMG_DIR}/scatter_kendalls_tau_seq.pdf",
+    )
+    plot_results(
+        x_axis="macro_f1",
+        metrics=[
+            f"{metric_name}_kendalls_tau_seq_max" for metric_name in found_metrics
+        ],
+        data=data,
+        save_path=f"{IMG_DIR}/scatter_kendalls_tau_seq_max.pdf",
+    )
